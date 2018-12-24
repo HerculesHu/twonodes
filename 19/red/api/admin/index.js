35dcda223b53532c89e446a76d6e1149b7c133d4
HerculesHu <31501333@stu.zucc.edu.cn>
2018-12-24 13:49:49 +0800

    uibase

--- 19/red/api/admin/index.js
+++ 19/red/api/admin/index.js
@@ -34,7 +34,17 @@ module.exports = {
         var needsPermission = auth.needsPermission;
 
         var adminApp = express();
-
+        adminApp.all('*',function (req, res, next) {
+               res.header('Access-Control-Allow-Origin', '*');
+               res.header('Access-Control-Allow-Headers', 'Content-Type,Content-Length, Authorization, Accept, X-Requested-With , Node-RED-Deployment-Type');
+               res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE,OPTIONS');
+               if (req.method == 'OPTIONS') {
+                   res.send(200);
+               }
+               else {
+                   next();
+               }
+            });
         // Flows
         adminApp.get("/flows",needsPermission("flows.read"),flows.get,apiUtil.errorHandler);
         adminApp.post("/flows",needsPermission("flows.write"),flows.post,apiUtil.errorHandler);
